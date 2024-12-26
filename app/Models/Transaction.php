cd8bfaf55a13853b20b1ce0d8354a36df0347a01
irsanwoy <irsanwoy54@gmail.com>
2024-12-26 20:46:10 +0700

    make model

---
+++ app/Models/Transaction.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Transaction extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['date', 'branch_id', 'employee_id', 'total'];
+
+    public function branch()
+    {
+        return $this->belongsTo(Branch::class);
+    }
+
+    public function employee()
+    {
+        return $this->belongsTo(Employee::class);
+    }
+
+    public function transactionDetails()
+    {
+        return $this->hasMany(TransactionDetail::class);
+    }
+}
